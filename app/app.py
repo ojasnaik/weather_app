a445dc0e282713f4a89167470067d6f42a3d16da
ojasnaik <naikojas@gmail.com>
2024-02-13 11:25:59 -0800

    added tests and html page

--- app/app.py
+++ app/app.py
@@ -1,15 +1,29 @@
+from flask import Flask, request, render_template
+from weather import Weather
+class App:
+    def __init__(self, weather):
+        self.weather = weather
+        self.app = Flask(__name__)
+        self.configure_routes()
 
-from weather import main as get_weather
-from flask import Flask, render_template, request
-from dataclasses import asdict
-app = Flask(__name__)
+    def configure_routes(self):
+        @self.app.route('/', methods=['GET', 'POST'])
+        def index():
+            data = None
+            if request.method == 'POST':
+                city = request.form['city']
+                state = request.form['state']
+                country = request.form['country']
+                data = self.get_weather(city, state, country)
+            return render_template('index.html', data=data)
+            
+    def get_weather(self, city, state, country):
+        return self.weather.main(city, state, country)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    # return render_template('index.html')
-    if request.method == 'GET':
-        return asdict(get_weather("San Jose", "CA", "US"))
-
+    def run(self):
+        self.app.run(debug=True, host='0.0.0.0', port=5000)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    weather = Weather()
+    app = App(weather)
+    app.run()
--- app/weather.py
+++ app/weather.py
@@ -3,35 +3,32 @@ from dotenv import load_dotenv
 import os
 from weather_data import WeatherData
 
-load_dotenv()
-api_key = os.getenv('API_KEY')
 
-def get_lan_long(city_name, state_code, country_code, API_key):
-    # Get the latitude and longitude of a location
-    response = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&&appid={API_key}').json()
-    data = response[0]
-    lat, lon = data['lat'], data['lon']
-    return lat, lon
+class Weather:
+    def __init__(self):
+        load_dotenv()
+        self.api_key = os.getenv('API_KEY')
 
-def get_weather_data(lat, lon, API_key):
-    # Get the weather data of a location
-    response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}&units=imperial').json()
-    data = WeatherData(
-        main=response['weather'][0]['main'],
-        description=response['weather'][0]['description'],
-        icon=response['weather'][0]['icon'],
-        temp=response['main']['temp']
-    )
-    
-    return data
+    def get_lat_long(self, city_name, state_code, country_code, API_key):
+        # Get the latitude and longitude of a location
+        response = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&&appid={API_key}').json()
+        data = response[0]
+        lat, lon = data['lat'], data['lon']
+        return lat, lon
 
-def main(city_name, state_code, country_code):
-    lat, lon = get_lan_long(city_name, state_code, country_code, api_key)
-    weather_data = get_weather_data(lat, lon, api_key)
-    return weather_data
+    def get_weather_data(self, lat, lon, API_key):
+        # Get the weather data of a location
+        response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}&units=imperial').json()
+        data = WeatherData(
+            main=response['weather'][0]['main'],
+            description=response['weather'][0]['description'],
+            icon=response['weather'][0]['icon'],
+            temp=response['main']['temp']
+        )
+        
+        return data
 
-if __name__ == '__main__':
-    lat, lon = get_lan_long('New York', 'NY', 'US', api_key)
-    weather_data = get_weather_data(lat, lon, api_key)
-    print(weather_data)
-    
+    def main(self, city_name, state_code, country_code):
+        lat, lon = self.get_lat_long(city_name, state_code, country_code, self.api_key)
+        weather_data = self.get_weather_data(lat, lon, self.api_key)
+        return weather_data
